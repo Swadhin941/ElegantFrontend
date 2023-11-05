9fcef92f1741d2edc2378b6e33295dcd733a772d
Swadhin941 <swadhinghose@gmail.com>
2023-11-05 13:45:30 +0600

    Fourth commit

--- src/Components/Home/TopBanner/TopBanner.js
+++ src/Components/Home/TopBanner/TopBanner.js
@@ -9,7 +9,7 @@ const TopBanner = () => {
                     <img src="https://i.ibb.co/gmvQcYh/Paste-image-removebg-preview-1.png" alt="" className='img-fluid' />
                 </div>
                 <div className="topBannerTextDiv">
-                    <h1 className='mb-0' style={{fontWeight:"700"}}>Listen to the amazing music sound</h1>
+                    <h1 className='mb-0' style={{fontWeight:"700"}}>Listen to the <span style={{color:"#377DFF"}}>amazing</span> music sound</h1>
                     <p className='mt-0' style={{fontWeight:"600"}}><small>Experience music like never before</small></p>
                     <div>
                         <button className='btn btn-dark w-75'>Shopping Now</button>
