c1cefe5f4fdc3cd1bd6149f84ef7f563e31c2d21
Swadhin941 <swadhinghose@gmail.com>
2023-11-05 13:31:28 +0600

    second commit

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -4,7 +4,7 @@ import useTitle from '../../CustomHook/useTitle/useTitle';
 const Home = () => {
     useTitle('3legant- Home')
     return (
-        <div className='container-fluid'>
+        <div className='container-fluid p-0'>
             <div className="row">
                 <div className="col-12 col-md-12 col-lg-12">
                     <TopBanner></TopBanner>
--- src/Components/Home/TopBanner/TopBanner.js
+++ src/Components/Home/TopBanner/TopBanner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import "./TopBanner.css";
 
 const TopBanner = () => {
     return (
-        <div>
-            <h1>Top Banner</h1>      
+        <div className='p-0 topBanner'>
+            <div className='topBannerContent'>
+                <div className="topBannerImgDiv">
+                    <img src="https://i.ibb.co/gmvQcYh/Paste-image-removebg-preview-1.png" alt="" className='img-fluid' />
+                </div>
+                <div className="topBannerTextDiv">
+                    <h1 className='mb-0' style={{fontWeight:"700"}}>Listen to the amazing music sound</h1>
+                    <p className='mt-0' style={{fontWeight:"600"}}><small>Experience music like never before</small></p>
+                </div>
+            </div>
         </div>
     );
 };
