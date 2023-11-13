cadf6a39adf2479d9f3e81a543b0cfbd03501af9
Swadhin941 <swadhinghose@gmail.com>
2023-11-13 21:48:37 +0600

    Authentication and authorization first commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
                     })
             })
             .catch(error => {
-                console.log(error.message);
+                toast.error(error.message);
             })
     }
 
